95ed644fdb85d6f375ea545e11f89d68b0caafbb
Mohit Bhat <159945664+Marco3041@users.noreply.github.com>
2025-10-22 17:12:25 +0530

    Delete terraform/terraform directory

--- terraform/terraform/keypair.tf
+++
@@ -1,8 +0,0 @@
-# ----------------------------
-# Key Pair for EC2
-# ----------------------------
-
-resource "aws_key_pair" "devops_key" {
-  key_name   = var.key_name
-  public_key = file(var.public_key_path)
-}
