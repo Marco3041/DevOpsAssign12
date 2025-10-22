3870d9011525fbbb8170828b829693ea9d30c0de
Mohit Bhat <159945664+Marco3041@users.noreply.github.com>
2025-10-22 17:10:41 +0530

    Delete terraform/terraform directory

--- terraform/terraform/provider.tf
+++
@@ -1,9 +0,0 @@
-# ----------------------------
-# AWS Provider
-# ----------------------------
-
-provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
